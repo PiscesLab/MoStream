4f87b82fb37537091cb4f534d70c2cf8f3c1c743
nomadicfoe <sumanthpaila1@gmail.com>
2025-10-27 14:55:44 -0700

    Add MoStream(1) content on new branch, excluding large files

--- MoStream/WLGenerator-node1/compute_ip_controller.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-#subprocess.run(["python", "simulator.py"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-#subprocess.run(["python", "compute_ip_parsl.py"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-subprocess.run(["python", "compute_ip_parsl_multiworkers.py"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
--- MoStream/WLGenerator-node1/stop_exp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pkill -9 python
-pkill -9 process_worker_
-pkill -9 parsl
-
